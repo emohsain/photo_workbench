b06615f313e8b29af88e646b4e491a497494281a
emohsain <emohsain@gmail.com>
2026-07-28 14:02:08 -0700

    add http Basic Auth using Rails encrypted credentials

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -4,3 +4,6 @@ def destroy
   redirect_to photos_path, notice: "Photo deleted successfully."
 end
 
+http_basic_authenticate_with name: Rails.application.credentials.photo_auth[:username],
+                              password: Rails.application.credentials.photo_auth[:password]
+
--- app/models/photos.rb
+++ app/models/photos.rb
@@ -3,5 +3,4 @@ class Photo < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 1000 }, allow_blank: true
-
 end
